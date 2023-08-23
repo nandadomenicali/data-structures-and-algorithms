7016227879e79b549235bd11f8479d6e2be4eff5
Fernanda Domenicali <43413565+nandadomenicali@users.noreply.github.com>
2023-08-23 11:21:13 -0300

    (feat): big o: o(n) datastructure

---
+++ src/main/java/datastructures/big_o/Main.java
@@ -0,0 +1,15 @@
+package main.java.datastructures.big_o;
+
+public class Main {
+
+    public static void printItems(int n) {
+        for(int i = 0; i < n; i++){
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        printItems(10);
+    }
+
+}
