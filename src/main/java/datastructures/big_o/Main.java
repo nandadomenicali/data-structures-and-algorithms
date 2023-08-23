72cf461875e6dee3e0a2e990c20d2ae3396a4e74
Fernanda Domenicali <43413565+nandadomenicali@users.noreply.github.com>
2023-08-23 11:27:05 -0300

    (feat): big o: o(n) drop constants

--- src/main/java/datastructures/big_o/Main.java
+++ src/main/java/datastructures/big_o/Main.java
@@ -6,6 +6,10 @@ public class Main {
         for(int i = 0; i < n; i++){
             System.out.println(i);
         }
+
+        for(int j = 0; j < n; j++){
+            System.out.println(j);
+        }
     }
 
     public static void main(String[] args) {
