ecef046b53201728d60aae66432b010fe2e8eba5
Fernanda Domenicali <43413565+nandadomenicali@users.noreply.github.com>
2023-08-23 11:30:36 -0300

    (feat): big o: o(nˆ2) datastructure

--- src/main/java/datastructures/big_o/Main.java
+++ src/main/java/datastructures/big_o/Main.java
@@ -12,8 +12,17 @@ public class Main {
         }
     }
 
+    public static void printAnotherItems(int n) {
+        for(int i = 0; i < n; i++){
+           for(int j = 0; j < n; j++){
+               System.out.println(i + " " + j);
+           }
+        }
+    }
+
     public static void main(String[] args) {
-        printItems(10);
+        //printItems(10);
+        printAnotherItems(10);
     }
 
 }
